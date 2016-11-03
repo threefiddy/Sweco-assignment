b35899c50a3c4df8d735f5781eaaa849d8a8c7d2
threefiddy <oeland13@hotmail.com>
2016-11-03 15:42:41 +0100

    updating

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-/*	var persons={"persons":[
-	{"ID":"1", "firstName":"Thomas", "lastName":"Robbinson", "age":"50", "gender":"male", "competences":["Matematisk klog", "fysisk stærk"]},
-	{"ID":"2","firstName":"John", "lastName":"Smith", "age":"45", "gender":"male", "competences":["fysisk stærk", "nysgerrig"]},
-	{"ID":"3","firstName":"Barack", "lastName":"Obama", "age":"39", "gender":"male", "competences":["Selvsikker", "Uafhængig af andre", "nysgerrig"]},
-	{"ID":"4","firstName":"Hillary", "lastName":"Clinton", "age":"48", "gender":"female","competences":["god til mennesker", "omsorgsfuld"]}
-	]}
+/*Udkommentér herfra og ned til der står hertil i kommentar. Er for at have nogle data i Localstorage når man starter.*/
+var persons={"persons":[
+{"ID":"1", "firstName":"Thomas", "lastName":"Robbinson", "age":"50", "gender":"male", "competences":["Matematisk klog", "fysisk stærk"]},
+{"ID":"2","firstName":"John", "lastName":"Smith", "age":"45", "gender":"male", "competences":["fysisk stærk", "nysgerrig"]},
+{"ID":"3","firstName":"Barack", "lastName":"Obama", "age":"39", "gender":"male", "competences":["Selvsikker", "Uafhængig af andre", "nysgerrig"]},
+{"ID":"4","firstName":"Hillary", "lastName":"Clinton", "age":"48", "gender":"female","competences":["god til mennesker", "omsorgsfuld"]}
+]}
 
-	localStorage.setItem('persons', JSON.stringify(persons));*/
+localStorage.setItem('persons', JSON.stringify(persons));
+/*hertil. Ellers vil localstorage hele tiden blive overskrevet, når man creater en ny person.*/
+
+
+
 
 var jsonList = JSON.parse(localStorage.getItem('persons'));
-/*console.log(localStorage.getItem('persons'));
-console.log(jsonList.persons[0]);*/
-
 for(i=0; i < jsonList.persons.length; i++)
 {
 	$('.container').append("<ul class='mainUl'>"+jsonList.persons[i].firstName+"<div class='listSpacing'></div>"+jsonList.persons[i].lastName + "<div class='listSpacing competences'>"+ jsonList.persons[i].competences[0] +"<br>"+ (jsonList.persons[i].competences[1]==undefined?"":jsonList.persons[i].competences[1])+"<br>"+ (jsonList.persons[i].competences[2]==undefined?"":jsonList.persons[i].competences[2]) +"</div><button type='button' class='btn btn-default showAllBtn' data-link='details.html?personId="+jsonList.persons[i].ID+"'>Vis detaljer</button> </ul>");
